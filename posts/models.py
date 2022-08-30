5d5ed1d2758f9a12bfb57d3efae0e3d029992e6d
Chris Coffman <chris.coffman2@gmail.com>
2022-08-30 11:34:29 -0500

    added comment system

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings 
 from django.db import models 
 from django.urls import reverse 
+from jiffygram.accounts.models import CustomUser 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -10,6 +11,7 @@ class Post(models.Model):
         on_delete=models.CASCADE)
     pet_image = models.ImageField(null = True, blank=True, upload_to="images/") 
     pet_name = models.CharField(max_length= 250, null=True, blank=True)
+    likes = models.ManyToManyField(CustomUser, related_name='jiffygram_posts' )
 
     class Meta:
         ordering = ['-date'] 
@@ -19,3 +21,18 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={'pk': self.pk}) 
+
+
+class Comment(models.Model): 
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140) 
+    author = models.ForeignKey( 
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.comment 
+
+    def get_absolute_url(self): 
+        return reverse('post_list') 
--- posts/views.py
+++ posts/views.py
@@ -1,21 +1,58 @@
 from django.views.generic.edit import UpdateView, DeleteView, CreateView 
-from django.views.generic import ListView, DetailView
-from django.urls import reverse_lazy 
+from django.views import View 
+from django.views.generic import ListView, DetailView, FormView 
+from django.views.generic.detail import SingleObjectMixin
+from django.urls import reverse_lazy, reverse 
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin
 )
 
 from .models import Post 
+from .forms import CommentForm 
+
+class CommentGet(DetailView):
+    model = Post 
+    template_name = 'post_detail.html' 
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm 
+        return context 
+
+class CommentPost(SingleObjectMixin, FormView):
+    model = Post 
+    form_class = CommentForm 
+    template_name = 'post_detail.html'
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object() 
+        return super().post(request, *args, **kwargs) 
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        comment = form.save(commit=False) 
+        comment.post = self.object 
+        comment.save() 
+        return super().form_valid(form) 
+    
+    def get_success_url(self):
+        post = self.get_object() 
+        return reverse("post_detail", kwargs={"pk": post.pk})
 
 class PostListView(LoginRequiredMixin, ListView):
     model = Post 
     template_name = 'post_list.html'
     queryset = Post.objects.order_by('-date') 
 
-class PostDetailView(LoginRequiredMixin, DetailView):
-    model = Post 
-    template_name = 'post_detail.html'
+class PostDetailView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view() 
+        return view(request, *args, **kwargs) 
+    
+    def post(self, request, *args, **kwargs):
+        view= CommentPost.as_view() 
+        return view(request, *args, **kwargs) 
 
 
 class PostUpdateView(
